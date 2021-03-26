77a80593f23e86df3111809a5f559cc18ff91f86
nevmstas <nevmyvakastas@gmail.com>
2021-03-26 23:47:42 +0600

    slug part, finished tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { css } from "@emotion/react";
-import { rhythm } from "../utils/typography";
-import Layout from "../components/layout";
+import React from "react"
+import { css } from "@emotion/react"
+import { Link, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
 
 export default function Home({ data }) {
-  console.log(data);
   return (
     <Layout>
       <div>
@@ -20,31 +19,39 @@ export default function Home({ data }) {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
+              <h3
                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #555;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
     </Layout>
-  );
+  )
 }
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -53,9 +60,12 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
           }
+          fields {
+            slug
+          }
           excerpt
         }
       }
     }
   }
-`;
+`
